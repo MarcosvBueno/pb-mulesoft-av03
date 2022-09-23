92b1be47128f48f23d52d085b3b406c7dd466425
MarcosvBueno <mvbueno07@gmail.com>
2022-09-23 19:42:42 -0300

    adiciona validacao de token ,autenticacao do cliente via token e ajustes no security

--- estados/src/main/java/org/example/estados/config/security/SecurityConfigurations.java
+++ estados/src/main/java/org/example/estados/config/security/SecurityConfigurations.java
@@ -1,10 +1,12 @@
 package org.example.estados.config.security;
 
+import org.example.estados.repository.UsuariosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -12,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -19,6 +22,12 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private AutenticacaoService autenticacaoService;
+	
+	@Autowired
+	private TokenService tokenService;
+	
+	@Autowired
+	private UsuariosRepository usuariosRepository;
 	
 	@Override
 	@Bean
@@ -36,12 +45,14 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
+		.antMatchers(HttpMethod.POST,"/api/v1/usuarios").permitAll()
 		.antMatchers(HttpMethod.POST, "/api/v1/autenticacao").permitAll()
-		.antMatchers(HttpMethod.GET,"api/v1/estados").permitAll()
-		.antMatchers(HttpMethod.GET, "api/v1/estados/*").permitAll()
+		.antMatchers(HttpMethod.GET,"/api/v1/estados").permitAll()
+		.antMatchers(HttpMethod.GET, "/api/v1/estados/*").permitAll()
 		.anyRequest().authenticated()
 		.and().csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuariosRepository), UsernamePasswordAuthenticationFilter.class);
 		
 	}
 	
--- estados/src/main/java/org/example/estados/controller/AutenticacaoController.java
+++ estados/src/main/java/org/example/estados/controller/AutenticacaoController.java
@@ -3,6 +3,7 @@ package org.example.estados.controller;
 import javax.validation.Valid;
 
 import org.example.estados.config.security.TokenService;
+import org.example.estados.controller.dto.TokenDto;
 import org.example.estados.controller.form.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -26,14 +27,13 @@ public class AutenticacaoController {
 	private TokenService  tokenService;
 
 	@PostMapping
-	public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form ){
+	public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form ){
 		UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 		
 		try {
 			Authentication authentication = authManager.authenticate(dadosLogin);
 			String token = tokenService.gerarToken(authentication);
-			System.out.println(token);
-			return ResponseEntity.ok().build();		
+			return ResponseEntity.ok(new TokenDto(token, "Bearer"));		
 		} catch (AuthenticationException e) {
 			return ResponseEntity.badRequest().build();
 		}
