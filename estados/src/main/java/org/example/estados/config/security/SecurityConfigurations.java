7a6c95e18aff4836ffd0141588548340617a5e64
MarcosvBueno <mvbueno07@gmail.com>
2022-09-23 14:14:05 -0300

    adicionando a dependencia e a classe de seguranca

---
+++ estados/src/main/java/org/example/estados/config/security/SecurityConfigurations.java
@@ -0,0 +1,11 @@
+package org.example.estados.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+}
